934426b41ea87b57371b4399f79ab9cc00329915
doi-doi <wilfredlsc@gmail.com>
2023-07-17 17:48:15 +0800

    Update statistical_arbitrage.py
    123

--- scripts/statistical_arbitrage.py
+++ scripts/statistical_arbitrage.py
@@ -1,77 +1,88 @@
 from decimal import Decimal
 
+import pandas as pd
+import pandas_ta as ta
+
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesFactory
 from hummingbot.strategy.directional_strategy_base import DirectionalStrategyBase
 
 
-class StatisticalArbitrage(DirectionalStrategyBase):
-    directional_strategy_name: str = "statistical_arbitrage"
+class StatisticalArbitrageLeft(DirectionalStrategyBase):
+    """
+    BotCamp Cohort #5 July 2023
+    Design Template: https://github.com/hummingbot/hummingbot-botcamp/issues/48
+
+    Description:
+    Statistical Arbitrage strategy implementation based on the DirectionalStrategyBase.
+    This strategy execute trades based on the Z-score values.
+    This strategy is divided into a left and right side code.
+    Left side code is statistical_arbitrage_left.py.
+    Right side code is statistical_arbitrage_right.py.
+    This code the left side of this strategy
+    When z-score indicates an entry signal. the left side will execute a long position and right side will execute a short position.
+    When z-score indicates an exit signal. the left side will execute a short position and right side will execute a long position.
+    """
+    directional_strategy_name: str = "statistical_arbitrage_left"
     # Define the trading pair and exchange that we want to use and the csv where we are going to store the entries
-    trading_pair: str = "ETH-USDT"
-    trading_pair_2: str = "BTC-USDT"
+    trading_pair: str = "ETH-USDT"  # left side trading pair
+    trading_pair_2: str = "BTC-USDT"  # right side trading pair
     exchange: str = "binance_perpetual"
-    order_amount_usd = Decimal("15")
+    order_amount_usd = Decimal("10")
     leverage = 10
+    length = 100
 
     # Configure the parameters for the position
-    stop_loss: float = 0.0075
-    take_profit: float = 0.015
-    time_limit: int = None
-    trailing_stop_activation_delta = 0.004
-    trailing_stop_trailing_delta = 0.001
+    zscore_entry: int = -2
+    zscore_entry_sl: int = -3
+    zscore_exit: int = 2
+    zscore_exit_sl: int = 3
 
     candles = [
         CandlesFactory.get_candle(connector=exchange,
                                   trading_pair=trading_pair,
-                                  interval="1m", max_records=150),
+                                  interval="1h", max_records=1000),
         CandlesFactory.get_candle(connector=exchange,
                                   trading_pair=trading_pair_2,
-                                  interval="1m", max_records=150),
+                                  interval="1h", max_records=1000),
     ]
-    markets = {exchange: {trading_pair}}
+    markets = {exchange: {trading_pair, trading_pair_2}}
 
     def get_signal(self):
 
-        signals = []
-        for candle in self.candles:
-            candles_df = self.get_processed_df(candle.candles_df)
-            last_row = candles_df.iloc[-1]
+        candles_df = self.get_processed_df()
+        z_score = candles_df.iat[-1, -1]
 
-            signal_value = last_row["ZS_21"].item()
-
-            signals.append(signal_value)
-
-        if signals[0] > 2:
-            return 0  # 1
-        elif signals[0] < -2:
-            return 0  # -1
+        # all execution are only on the left side trading pair
+        if z_score < self.zscore_entry or z_score > self.zscore_exit_sl:
+            return 1  # long 1
+        elif z_score < self.zscore_entry_sl or z_score > self.zscore_exit:
+            return -1  # stop loss long  -1
         else:
             return 0
 
-    @staticmethod
-    def get_processed_df(candles):
+    def get_processed_df(self):
 
-        candles_df = candles.copy()
-        # calc z-score
-        # model = sm.OLS(candles[0], candles[1]).fit()
-        # hedge_ratio = model.params[0]
-        # spread = pd.Series(candles[0]) - (pd.Series(candles[1]) * hedge_ratio)
-        # spread_df = pd.DataFrame(spread)
-        # candles_df.ta.zscore(spread_df, length=21, append=True)
-        candles_df.ta.zscore(length=21, append=True)
+        candles_df_1 = self.candles[0].candles_df
+        candles_df_2 = self.candles[1].candles_df
 
-        return candles_df
+        # calculate the spread and z-score based on the candles of 2 trading pairs
+        df = pd.merge(candles_df_1, candles_df_2, on="timestamp", how='inner', suffixes=('', '_2'))
+        hedge_ratio = df["close"].tail(self.length).mean() / df["close_2"].tail(self.length).mean()
+
+        df["spread"] = df["close"] - (df["close_2"] * hedge_ratio)
+        df["z_score"] = ta.zscore(df["spread"], length=self.length)
+
+        return df
 
     def market_data_extra_info(self):
         """
-        Provides additional information about the market data for each candlestick.
+        Provides additional information about the market data to the format status.
         Returns:
             List[str]: A list of formatted strings containing market data information.
         """
         lines = []
-        columns_to_show = ["timestamp", "open", "low", "high", "close", "volume", "ZS_21"]
-        for candle in self.candles:
-            candles_df = self.get_processed_df(candle.candles_df)
-            lines.extend([f"Candles: {candle.name} | Interval: {candle.interval}\n"])
-            lines.extend(self.candles_formatted_list(candles_df, columns_to_show))
+        columns_to_show = ["timestamp", "open", "low", "high", "close", "volume", "z_score", "close_2"]
+        candles_df = self.get_processed_df()
+        lines.extend([f"Candles: {self.candles[0].name} | Interval: {self.candles[0].interval}\n"])
+        lines.extend(self.candles_formatted_list(candles_df, columns_to_show))
         return lines
